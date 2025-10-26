a665f8a579f60f35b0c1ec51161f491990cc93d1
shyam sundar <sundarshyam27241@gmail.com>
2025-10-26 13:00:42 +0530

    db.js changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 const mongodbURI = "mongodb+srv://sundarshyam27241_db_user:7yIJerJ7jW2Qd99o@cluster0.5j0siyw.mongodb.net/sampleDB?appName=Cluster0";
 
-const connectDB = () => {
-    mongoose.connect(mongodbURI).then(() => {
-        console.log("Connected to MongoDB");
-    }).catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-    });
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(mongodbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        return conn;
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        throw error;
+    }
 };
 
 export default connectDB;
